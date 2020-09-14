594f0d3d0c071f1b13de4f90fc71991b64c287a8
Julius Wallnöfer <julius.wallnoefer@outlook.com>
2020-09-14 10:03:46 +0200

    update run and plot files

--- run/run_whitepaper_nsp.py
+++ run/run_whitepaper_nsp.py
@@ -15,12 +15,12 @@ params_available_SiV = {"P_LINK": 5 * 10**-2,
                         "f_clock": 30 * 10**6,
                         "T_DP": 1 * 10**-3}
 params_available_Qdot = {"P_LINK": 10 * 10**-2,
-                          "f_clock": 1000 * 10**6,
-                          "T_DP": 0.003 * 10**-3}
-params_available_Ca = {"P_LINK": 0.4 * 10**-2,
-                       "f_clock": 0.06 * 10**6,
-                       "T_DP": 0.8 * 10**-3}
-params_available_Rb = {"P_LINK": 70 * 10**-2,
+                         "f_clock": 1000 * 10**6,
+                         "T_DP": 0.003 * 10**-3}
+params_available_Ca = {"P_LINK": 25 * 10**-2,
+                       "f_clock": 0.47 * 10**6,
+                       "T_DP": 20 * 10**-3}
+params_available_Rb = {"P_LINK": 50 * 10**-2,
                        "f_clock": 5 * 10**6,
                        "T_DP": 100 * 10**-3}
 params_future_NV = {"P_LINK": 50 * 10**-2,
@@ -30,50 +30,112 @@ params_future_SiV = {"P_LINK": 50 * 10**-2,
                      "f_clock": 500 * 10**6,
                      "T_DP": 100 * 10**-3}
 params_future_Qdot = {"P_LINK": 60 * 10**-2,
-                       "f_clock": 1000 * 10**6,
-                       "T_DP": 0.3 * 10**-3}
-params_future_Ca = {"P_LINK": 10 * 10**-2,
-                    "f_clock": 1 * 10**6,
-                    "T_DP": 1 * 10**-3}
+                      "f_clock": 1000 * 10**6,
+                      "T_DP": 0.3 * 10**-3}
+params_future_Ca = {"P_LINK": 50 * 10**-2,
+                    "f_clock": 10 * 10**6,
+                    "T_DP": 300 * 10**-3}
 params_future_Rb = {"P_LINK": 70 * 10**-2,
-                    "f_clock": 100 * 10**6,
+                    "f_clock": 10 * 10**6,
                     "T_DP": 1000 * 10**-3}
 
 available_params = [params_available_NV, params_available_SiV, params_available_Qdot, params_available_Ca, params_available_Rb]
 future_params = [params_future_NV, params_future_SiV, params_future_Qdot, params_future_Ca, params_future_Rb]
 name_list = ["NV", "SiV", "Qdot", "Ca", "Rb"]
+ms_available = [25, 10, 0, 20, 100]  # # 25/20/0/100/10 for NV/Ca/Qdot/Rb/SiV (current values on the left) and
+ms_future = [5000, 50, 0, 200, 500]  # #5000/200/0/500/50 for NV/Ca/Qdot/Rb/SiV (future values on the right).
+
+
 
 if __name__ == "__main__":
     length_list = np.arange(25000, 425000, 25000)
-    # for name, params in zip(name_list, available_params):
+    # for name, params, m in zip(name_list, available_params, ms_available):
+    #     if name == "Qdot":
+    #         continue
     #     print(name)
     #     key_per_time_list = []
     #     key_per_resource_list = []
     #     for l in length_list:
     #         print(l)
-    #         p = run(length=l, max_iter=10000, params=params, mode="sim")
+    #         trial_time_manual = l / C
+    #         p = run(length=l, max_iter=10000, params=params, cutoff_time=m*trial_time_manual + 10**-6*trial_time_manual, mode="sim")
     #         key_per_time = calculate_keyrate_time(p.correlations_z_list, p.correlations_x_list, 1, p.world.event_queue.current_time + 2 * l / C)
     #         key_per_resource = calculate_keyrate_channel_use(p.correlations_z_list, p.correlations_x_list, 1, p.resource_cost_max_list)
     #         key_per_time_list += [key_per_time]
     #         key_per_resource_list += [key_per_resource]
+    #         if (10 * np.log10(key_per_resource / 2)) < (-60):
+    #             break
     #     path = os.path.join(result_path, "available", name)
     #     assert_dir(path)
-    #     np.savetxt(os.path.join(path, "length_list.txt"), length_list)
+    #     np.savetxt(os.path.join(path, "length_list.txt"), length_list[:len(key_per_resource_list)])
     #     np.savetxt(os.path.join(path, "key_per_time_list.txt"), key_per_time_list)
     #     np.savetxt(os.path.join(path, "key_per_resource_list.txt"), key_per_resource_list)
-    for name, params in zip(name_list, future_params):
+    for name, params, m in zip(name_list, future_params, ms_future):
+        if name == "Qdot":
+            continue
         print(name)
         key_per_time_list = []
         key_per_resource_list = []
         for l in length_list:
             print(l)
-            p = run(length=l, max_iter=10000, params=params, mode="sim")
+            trial_time_manual = l / C
+            p = run(length=l, max_iter=10000, params=params, cutoff_time=m*trial_time_manual + 10**-6*trial_time_manual, mode="sim")
             key_per_time = calculate_keyrate_time(p.correlations_z_list, p.correlations_x_list, 1, p.world.event_queue.current_time + 2 * l / C)
             key_per_resource = calculate_keyrate_channel_use(p.correlations_z_list, p.correlations_x_list, 1, p.resource_cost_max_list)
             key_per_time_list += [key_per_time]
             key_per_resource_list += [key_per_resource]
+            if (10 * np.log10(key_per_resource / 2)) < (-60):
+                break
         path = os.path.join(result_path, "future", name)
         assert_dir(path)
-        np.savetxt(os.path.join(path, "length_list.txt"), length_list)
+        np.savetxt(os.path.join(path, "length_list.txt"), length_list[:len(key_per_resource_list)])
         np.savetxt(os.path.join(path, "key_per_time_list.txt"), key_per_time_list)
         np.savetxt(os.path.join(path, "key_per_resource_list.txt"), key_per_resource_list)
+
+    # ### here we plot the Rb lines for different cut-off times
+    # name = "Rb"
+    # params = params_available_Rb
+    # length_list = np.arange(25000, 425000, 25000)
+    # for m in [200, 500, 1000, 2000, 5000, 10000, 20000]:
+    #     print("m=%d" % m)
+    #     key_per_time_list = []
+    #     key_per_resource_list = []
+    #     for l in length_list:
+    #         print(l)
+    #         trial_time_manual = l / C
+    #         p = run(length=l, max_iter=10000, params=params, cutoff_time=m*trial_time_manual + 10**-6*trial_time_manual, mode="sim")
+    #         key_per_time = calculate_keyrate_time(p.correlations_z_list, p.correlations_x_list, 1, p.world.event_queue.current_time + 2 * l / C)
+    #         key_per_resource = calculate_keyrate_channel_use(p.correlations_z_list, p.correlations_x_list, 1, p.resource_cost_max_list)
+    #         key_per_time_list += [key_per_time]
+    #         key_per_resource_list += [key_per_resource]
+    #     path = os.path.join(result_path, "available", "m_test", name)
+    #     assert_dir(path)
+    #     np.savetxt(os.path.join(path, "length_list_%d.txt" % m), length_list)
+    #     np.savetxt(os.path.join(path, "key_per_time_list_%d.txt" % m), key_per_time_list)
+    #     np.savetxt(os.path.join(path, "key_per_resource_list_%d.txt" % m), key_per_resource_list)
+
+    # # ### further investigate cutoff times - especially the claim that you can set it too low
+    # # ### effect should be very visible if memory quality is very high
+    # test_params = {"P_LINK": 10 * 10**-2,
+    #                "T_DP": 1}
+    # length = 22 * 10**3
+    # trial_time_manual = length / C
+    # m_list = [m for m in range(1, 41, 2)]
+    # cutoff_list = [m * trial_time_manual + 10**-6 * trial_time_manual for m in m_list]
+    # key_per_time_list = []
+    # key_per_resource_list = []
+    # for m, cutoff_time in zip(m_list, cutoff_list):
+    #     print(m)
+    #     p = run(length=length, max_iter=10000, params=test_params, cutoff_time=cutoff_time, mode="sim")
+    #     key_per_time = calculate_keyrate_time(p.correlations_z_list, p.correlations_x_list, 1, p.world.event_queue.current_time + 2 * length / C)
+    #     key_per_resource = calculate_keyrate_channel_use(p.correlations_z_list, p.correlations_x_list, 1, p.resource_cost_max_list)
+    #     key_per_time_list += [key_per_time]
+    #     key_per_resource_list += [key_per_resource]
+    # path = os.path.join(result_path, "cutoff_test")
+    # assert_dir(path)
+    # np.savetxt(os.path.join(path, "m_list.txt"), m_list)
+    # np.savetxt(os.path.join(path, "key_per_time_list.txt"), key_per_time_list)
+    # np.savetxt(os.path.join(path, "key_per_resource_list.txt"), key_per_resource_list)
+    # plt.plot(m_list, key_per_resource_list)
+    # plt.grid()
+    # plt.show()
