a16d57350f13bebbb3a400cac5c7708c90076bb8
Julius Wallnöfer <julius.wallnoefer@outlook.com>
2020-09-30 18:31:09 +0200

    new run with worse memories

--- run/run_two_link_epp_comparison.py
+++ run/run_two_link_epp_comparison.py
@@ -8,13 +8,31 @@ C = 2 * 10**8  # speed of light in optical fiber
 
 result_path = os.path.join("results", "two_link_epp")
 
-
-params = {"P_LINK": 50 * 10**-2,
+params = {"P_LINK": 30 * 10**-2,
           "f_clock": 5 * 10**6,
-          "T_DP": 100 * 10**-3}
+          "T_DP": 10 * 10**-3}
 
+# ETA_P = 0.66  # preparation efficiency
+# T_P = 2 * 10**-6  # preparation time
+# ETA_C = 0.04 * 0.3  # phton-fiber coupling efficiency * wavelength conversion
+# T_2 = 1  # dephasing time
+# E_M_A = 0.01  # misalignment error
+# P_D = 10**-8  # dark count probability per detector
+# ETA_D = 0.3  # detector efficiency
+# P_BSM = 1  # BSM success probability  ## WARNING: Currently not implemented
+# LAMBDA_BSM = 0.97  # BSM ideality parameter
+# # F = 1.16  # error correction inefficiency
+# ETA_TOT = ETA_P * ETA_C * ETA_D
+# luetkenhaus_params = {"P_LINK": ETA_TOT,
+#                       "T_P": T_P,
+#                       "T_DP": T_2,
+#                       "E_MA": E_M_A,
+#                       "P_D": P_D,
+#                       "LAMBDA_BSM": LAMBDA_BSM}
 
-length_list = np.arange(25000, 425000, 25000)
+length_list = np.concatenate([np.array([25000, 37500]), np.arange(50000, 125000, 10000), np.arange(125000, 425000, 25000)])
+# length_list = np.concatenate([np.arange(1000, 61000, 2500), np.arange(61000, 69000, 1000), np.arange(69000, 84000, 2500)])
+# params = luetkenhaus_params
 
 # run without epp first:
 print("Run without epp")
