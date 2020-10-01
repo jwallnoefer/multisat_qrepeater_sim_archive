33ddbc2d620d41189086d00ad9f2912c431f31f8
Julius Wallnöfer <julius.wallnoefer@outlook.com>
2020-10-01 13:30:06 +0200

    improved formatting

--- libs/aux_functions.py
+++ libs/aux_functions.py
@@ -13,25 +13,42 @@ dejmps_proj_ket_z1z1 = mat.tensor(mat.I(4), mat.z1, mat.z1)
 dejmps_proj_bra_z0z0 = mat.H(dejmps_proj_ket_z0z0)
 dejmps_proj_bra_z1z1 = mat.H(dejmps_proj_ket_z1z1)
 
+
 def binary_entropy(p):
     if p == 1 or p == 0:
         return 0
     else:
         return -p * np.log2(p) - (1 - p) * np.log2((1 - p))
 
+
 def calculate_keyrate_time(correlations_z, correlations_x, err_corr_ineff, time_interval):
-    e_z = 1 - np.sum(correlations_z)/len(correlations_z)
-    e_x = 1 - np.sum(correlations_x)/len(correlations_x)
+    e_z = 1 - np.sum(correlations_z) / len(correlations_z)
+    e_x = 1 - np.sum(correlations_x) / len(correlations_x)
     return len(correlations_z) / time_interval * (1 - binary_entropy(e_x) - err_corr_ineff * binary_entropy(e_z))
 
+
 def calculate_keyrate_channel_use(correlations_z, correlations_x, err_corr_ineff, resource_list):
-    e_z = 1 - np.sum(correlations_z)/len(correlations_z)
-    e_x = 1 - np.sum(correlations_x)/len(correlations_x)
+    e_z = 1 - np.sum(correlations_z) / len(correlations_z)
+    e_x = 1 - np.sum(correlations_x) / len(correlations_x)
     return len(correlations_z) / np.sum(resource_list) * (1 - binary_entropy(e_x) - err_corr_ineff * binary_entropy(e_z))
 
+
 def assert_dir(path):
+    """Check if `path` exists, and create it if it doesn't.
+
+    Parameters
+    ----------
+    path : str
+        The path to be checked/created.
+
+    Returns
+    -------
+    None
+
+    """
     if not os.path.exists(path):
         os.makedirs(path)
+
 
 def apply_single_qubit_map(map_func, qubit_index, rho, *args, **kwargs):
     """Applies a single-qubit map to a density matrix of n qubits.
@@ -58,10 +75,11 @@ def apply_single_qubit_map(map_func, qubit_index, rho, *args, **kwargs):
     rho = rho.reshape((2, 2) * n)
     # there must be a nicer way to do the iteration here:
     out = np.zeros_like(rho)
-    for idx in np.ndindex(*(2, 2)*(n-1)):
-        my_slice = idx[:qubit_index] + (slice(None),) + idx[qubit_index:n-1+qubit_index] + (slice(None),) + idx[n-1+qubit_index:]
+    for idx in np.ndindex(*(2, 2) * (n - 1)):
+        my_slice = idx[:qubit_index] + (slice(None),) + idx[qubit_index:n - 1 + qubit_index] + (slice(None),) + idx[n - 1 + qubit_index:]
         out[my_slice] = map_func(rho[my_slice], *args, **kwargs)
-    return out.reshape((2**n,2**n))
+    return out.reshape((2**n, 2**n))
+
 
 def x_noise_channel(rho, epsilon):
     """A single-qubit bit-flip channel.
@@ -81,6 +99,7 @@ def x_noise_channel(rho, epsilon):
     """
     return (1 - epsilon) * rho + epsilon * np.dot(np.dot(mat.X, rho), mat.H(mat.X))
 
+
 def y_noise_channel(rho, epsilon):
     """A single-qubit bit-and-phase-flip channel.
 
@@ -98,6 +117,7 @@ def y_noise_channel(rho, epsilon):
 
     """
     return (1 - epsilon) * rho + epsilon * np.dot(np.dot(mat.Y, rho), mat.H(mat.Y))
+
 
 def z_noise_channel(rho, epsilon):
     """A single-qubit phase-flip channel.
@@ -117,6 +137,7 @@ def z_noise_channel(rho, epsilon):
     """
     return (1 - epsilon) * rho + epsilon * np.dot(np.dot(mat.Z, rho), mat.H(mat.Z))
 
+
 def w_noise_channel(rho, alpha):
     """A single-qubit depolarizing (white) noise channel.
 
@@ -134,7 +155,8 @@ def w_noise_channel(rho, alpha):
         The density matrix with the map applied.
 
     """
-    return alpha * rho + (1 - alpha) * mat.I(2) / 2 * np.trace(rho) # trace is necessary if dealing with unnormalized states (e.g. in apply_single_qubit_map)
+    return alpha * rho + (1 - alpha) * mat.I(2) / 2 * np.trace(rho)  # trace is necessary if dealing with unnormalized states (e.g. in apply_single_qubit_map)
+
 
 def dejmps_protocol(rho):
     """Applies the DEJMPS entanglement purification protocol.
