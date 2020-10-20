384a6b68199965e7a3376c917dc28fb5c71bcc1f
Julius Wallnöfer <julius.wallnoefer@outlook.com>
2020-10-20 19:55:11 +0200

    adds tests for new station options

--- tests/test_quantum_objects.py
+++ tests/test_quantum_objects.py
@@ -5,6 +5,10 @@ from quantum_objects import Qubit, Pair, Station, Source, SchedulingSource, Worl
 from events import SourceEvent
 import numpy as np
 import libs.matrix as mat
+from noise import NoiseChannel
+from libs.aux_functions import w_noise_channel, apply_single_qubit_map
+
+test_noise_channel = NoiseChannel(n_qubits=1, channel_function=lambda rho: w_noise_channel(rho=rho, alpha=0.99))
 
 
 class TestQuantumObjects(unittest.TestCase):
@@ -45,14 +49,32 @@ class TestQuantumObjects(unittest.TestCase):
         self.assertNotIn(qubit, station.qubits)
 
     def test_cutoff_time_station(self):
-        cutoff_time = np.random.random()*40
+        cutoff_time = np.random.random() * 40
         station = Station(world=self.world, id=1, position=0, memory_cutoff_time=cutoff_time)
         qubit = station.create_qubit()
         self.assertIn(qubit, self.world.world_objects[qubit.type])
-        self.event_queue.resolve_until(cutoff_time - 10**-6*cutoff_time)
+        self.event_queue.resolve_until(cutoff_time - 10**-6 * cutoff_time)
         self.assertIn(qubit, self.world.world_objects[qubit.type])
         self.event_queue.resolve_until(cutoff_time)
         self.assertNotIn(qubit, self.world.world_objects[qubit.type])
+
+    def test_station_options(self):
+        # test dark_count_probability
+        station = Station(world=self.world, position=0)  # not specified
+        self.assertEqual(station.dark_count_probability, 0)
+        random_num = np.random.random()
+        station = Station(world=self.world, position=0, dark_count_probability=random_num)
+        self.assertEqual(station.dark_count_probability, random_num)
+        # test map on creation (which is used for alignment)
+        station = Station(world=self.world, position=0, creation_noise_channel=test_noise_channel)
+        test_state = np.random.random((4, 4))
+        test_state = 1 / 2 * (test_state + mat.H(test_state))
+        test_state = test_state / np.trace(test_state)
+        station_qubit = station.create_qubit()
+        other_qubit = Qubit(world=self.world, station=MagicMock(None))
+        pair = Pair(world=self.world, qubits=[station_qubit, other_qubit], initial_state=test_state)
+        expected_state = apply_single_qubit_map(map_func=lambda mu: w_noise_channel(rho=mu, alpha=0.99), qubit_index=0, rho=test_state)
+        self.assertTrue(np.allclose(pair.state, expected_state))
 
     def test_source(self):
         stations = [Station(world=self.world, id=i, position=200 * i) for i in range(2)]
@@ -78,14 +100,14 @@ class TestQuantumObjects(unittest.TestCase):
         start_time = self.world.event_queue.current_time
         # now schedule an event
         source.schedule_event()
-        event=self.world.event_queue.next_event
+        event = self.world.event_queue.next_event
         self.assertIsInstance(event, SourceEvent)
         self.assertEqual(event.time, start_time + 5)
         self.world.event_queue.resolve_next_event()
         pair = self.world.world_objects["Pair"][0]
         self.assertTrue(np.allclose(pair.state, np.dot(mat.phiplus, mat.H(mat.phiplus))))
         source.schedule_event()
-        event=self.world.event_queue.next_event
+        event = self.world.event_queue.next_event
         self.assertIsInstance(event, SourceEvent)
         self.assertEqual(event.time, start_time + 10)
 
