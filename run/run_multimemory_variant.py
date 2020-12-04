d7361faa3ad0a2200d7aab0ce9ef50468a469c1a
Julius Wallnöfer <julius.wallnoefer@outlook.com>
2020-12-04 17:20:42 +0100

    adjusted for running on cluster

--- run/run_multimemory_variant.py
+++ run/run_multimemory_variant.py
@@ -3,6 +3,8 @@ from scenarios.multi_memory_variant import run
 from libs.aux_functions import assert_dir, binary_entropy, calculate_keyrate_time, calculate_keyrate_channel_use
 import numpy as np
 import matplotlib.pyplot as plt
+from time import time
+from multiprocessing import Pool
 
 C = 2 * 10**8  # speed of light in optical fiber
 L_ATT = 22 * 10**3  # attenuation length
@@ -29,33 +31,83 @@ params = {"P_LINK": ETA_TOT,
           "P_D": P_D,
           "LAMBDA_BSM": LAMBDA_BSM}
 
-if __name__ == "__main__":
-    # length_list = np.arange(20000, 400000, 20000)
-    # length_list = np.arange(5000, 200000, 5000)
-    length_list = np.arange(10000, 400000, 10000)
-    # max_iter = 10000
-    max_iter = 10000
-    for num_memories in [1, 5, 10, 50, 100, 400]:
-        key_per_time_list = []
-        key_per_resource_list = []
-        from time import time
-        start_time = time()
-        for l in length_list:
-            print(l)
-            # BEGIN cutoff estimation
-            trial_time_manual = T_P + 2 * (l / 2) / C
-            expected_time = trial_time_manual / (ETA_TOT * np.exp(-(l / 2) / L_ATT))  # expected time ONE memory would take to have a successful pair
-            cutoff_time = 3 * expected_time
-            # END cutoff estimation
-            p = run(length=l, max_iter=max_iter, params=params, cutoff_time=cutoff_time, num_memories=num_memories)
-            key_per_time = calculate_keyrate_time(p.correlations_z_list, p.correlations_x_list, F, p.world.event_queue.current_time + 2 * l / C)
-            key_per_resource = calculate_keyrate_channel_use(p.correlations_z_list, p.correlations_x_list, F, p.resource_cost_max_list)
 
-            key_per_time_list += [key_per_time]
-            key_per_resource_list += [key_per_resource]
-            if key_per_resource < 0:
-                break
-            # print("l=%d took %s seconds" % (l, str(time() - start_time)))
+def do_the_thing(length, max_iter, params, cutoff_time, num_memories):
+    p = run(length=length, max_iter=max_iter, params=params, cutoff_time=cutoff_time, num_memories=num_memories)
+    key_per_time = calculate_keyrate_time(p.correlations_z_list, p.correlations_x_list, F, p.world.event_queue.current_time + 2 * length / C)
+    key_per_resource = calculate_keyrate_channel_use(p.correlations_z_list, p.correlations_x_list, F, p.resource_cost_max_list)
+    return key_per_time, key_per_resource
+
+if __name__ == "__main__":
+    # # length_list = np.arange(20000, 400000, 20000)
+    # # length_list = np.arange(5000, 200000, 5000)
+    # length_list = np.arange(10000, 400000, 10000)
+    # max_iter = 10000
+    # for num_memories in [1, 5, 10, 50, 100, 400]:
+    #     key_per_time_list = []
+    #     key_per_resource_list = []
+    #     from time import time
+    #     start_time = time()
+    #     for l in length_list:
+    #         print(l)
+    #         # BEGIN cutoff estimation
+    #         trial_time_manual = T_P + 2 * (l / 2) / C
+    #         expected_time = trial_time_manual / (ETA_TOT * np.exp(-(l / 2) / L_ATT))  # expected time ONE memory would take to have a successful pair
+    #         cutoff_time = 3 * expected_time
+    #         # END cutoff estimation
+    #         p = run(length=l, max_iter=max_iter, params=params, cutoff_time=cutoff_time, num_memories=num_memories)
+    #         key_per_time = calculate_keyrate_time(p.correlations_z_list, p.correlations_x_list, F, p.world.event_queue.current_time + 2 * l / C)
+    #         key_per_resource = calculate_keyrate_channel_use(p.correlations_z_list, p.correlations_x_list, F, p.resource_cost_max_list)
+    #
+    #         key_per_time_list += [key_per_time]
+    #         key_per_resource_list += [key_per_resource]
+    #         if key_per_resource < 0:
+    #             break
+    #         # print("l=%d took %s seconds" % (l, str(time() - start_time)))
+    #
+    #     output_path = os.path.join(result_path, "%d_memories" % num_memories)
+    #     assert_dir(output_path)
+    #
+    #     np.savetxt(os.path.join(output_path, "length_list.txt"), length_list[:len(key_per_resource_list)])
+    #     np.savetxt(os.path.join(output_path, "key_per_time_list.txt"), key_per_time_list)
+    #     np.savetxt(os.path.join(output_path, "key_per_resource_list.txt"), key_per_resource_list)
+    #
+    #     print("num_memories=%d took %s seconds" % (num_memories, str(time() - start_time)))
+    #
+    #     # plt.plot(length_list[:len(key_per_resource_list)], key_per_time_list)
+    #     # plt.yscale("log")
+    #     # plt.xlabel("total length")
+    #     # plt.ylabel("key_rate_per_time")
+    #     # plt.show()
+    #     #
+    #     # plt.plot(length_list[:len(key_per_resource_list)], key_per_resource_list)
+    #     # plt.yscale("log")
+    #     # plt.xlabel("total length")
+    #     # plt.ylabel("key rate per channel use")
+    #     # plt.show()
+
+    # cluster variant
+    num_processes = 20
+    length_list = np.arange(10000, 400000, 10000)
+    memories_list = [1, 5, 10, 50, 100, 400]
+    max_iter = 10000
+    res = {}
+    start_time = time()
+    pool = Pool(num_processes)
+    for num_memories in memories_list:
+        # BEGIN cutoff estimation
+        trial_time_manual = T_P + 2 * (length_list / 2) / C
+        expected_time = trial_time_manual / (ETA_TOT * np.exp(-(length_list / 2) / L_ATT))  # expected time ONE memory would take to have a successful pair
+        cutoff_time = 3 * expected_time
+        # END cutoff estimation
+        num_calls = len(length_list)
+        aux_list = zip(length_list, [max_iter] * num_calls, [params] * num_calls, cutoff_time, [num_memories] * num_calls)
+        res[num_memories] = pool.starmap_async(do_the_thing, aux_list)
+    pool.close()
+    pool.join()
+
+    for num_memories in memories_list:
+        key_per_time_list, key_per_resource_list = zip(*list(res[num_memories].get()))
 
         output_path = os.path.join(result_path, "%d_memories" % num_memories)
         assert_dir(output_path)
@@ -64,16 +116,4 @@ if __name__ == "__main__":
         np.savetxt(os.path.join(output_path, "key_per_time_list.txt"), key_per_time_list)
         np.savetxt(os.path.join(output_path, "key_per_resource_list.txt"), key_per_resource_list)
 
-        print("num_memories=%d took %s seconds" % (num_memories, str(time() - start_time)))
-
-        # plt.plot(length_list[:len(key_per_resource_list)], key_per_time_list)
-        # plt.yscale("log")
-        # plt.xlabel("total length")
-        # plt.ylabel("key_rate_per_time")
-        # plt.show()
-        #
-        # plt.plot(length_list[:len(key_per_resource_list)], key_per_resource_list)
-        # plt.yscale("log")
-        # plt.xlabel("total length")
-        # plt.ylabel("key rate per channel use")
-        # plt.show()
+    print("The whole run took %s seconds." % str(time() - start_time))
