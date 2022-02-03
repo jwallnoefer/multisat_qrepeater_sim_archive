ca670a4d8f0a6cf2aa5b997ce9a69d659117a608
Julius Wallnöfer <julius.wallnoefer@outlook.com>
2022-02-03 15:23:46 +0100

    explore one satellite ready

--- scenarios/one_satellite/explore_multimemory_satellite.py
+++ scenarios/one_satellite/explore_multimemory_satellite.py
@@ -1,44 +1,12 @@
 import os, sys; sys.path.insert(0, os.path.abspath("."))
 from scenarios.one_satellite.multi_memory_satellite import run
-from libs.aux_functions import assert_dir, standard_bipartite_evaluation, save_result
+from scenarios.three_satellites.common_params import base_params
+from libs.aux_functions import assert_dir, standard_bipartite_evaluation
 import numpy as np
 import matplotlib.pyplot as plt
 from time import time
-from multiprocessing import Pool
-import pandas as pd
 import pickle
 from consts import SPEED_OF_LIGHT_IN_VACCUM as C
-
-# # # values taken from https://arxiv.org/abs/2006.10636
-F_CLOCK = 20e6  # 20 MHz is very high
-T_P = 0  # preparation time
-E_M_A = 0  # misalignment error
-P_D = 10**-6  # dark count probability per detector
-P_BSM = 1  # BSM success probability  ## WARNING: Currently not implemented
-LAMBDA_BSM = 1  # BSM ideality parameter
-F = 1  # error correction inefficiency
-
-# T_2 = 1  # dephasing time
-ETA_MEM = 0.8  # memory efficiency
-ETA_DET = 0.7  # detector efficiency
-
-ORBITAL_HEIGHT = 400e3
-SENDER_APERTURE_RADIUS = 0.15
-RECEIVER_APERTURE_RADIUS = 0.50
-# DIVERGENCE_THETA = 10e-6
-
-P_LINK = ETA_MEM * ETA_DET
-base_params = {"P_LINK": P_LINK,
-               "ETA_MEM": ETA_MEM,
-               "ETA_DET": ETA_DET,
-               "T_P": T_P,
-               "F_CLOCK": F_CLOCK,
-               "E_MA": E_M_A,
-               "P_D": P_D,
-               "LAMBDA_BSM": LAMBDA_BSM,
-               "ORBITAL_HEIGHT": ORBITAL_HEIGHT,
-               "SENDER_APERTURE_RADIUS": SENDER_APERTURE_RADIUS,
-               "RECEIVER_APERTURE_RADIUS": RECEIVER_APERTURE_RADIUS}
 
 
 if __name__ == "__main__":
@@ -48,12 +16,12 @@ if __name__ == "__main__":
     if case_number in [1, 2, 3, 4]:
         # Case 1: The divergence plot - x-axis: length, thing we want to vary: thetas
         total_begin_time = time()
-        thetas = {1: 2e-6, 2: 4e-6, 3: 6e-6, 4: 8e-6}
+        thetas = {1: 3e-6, 2: 4e-6, 3: 6e-6, 4: 8e-6}
         params = dict(base_params)
         params["DIVERGENCE_THETA"] = thetas[case_number]
-        params["T_DP"] = 100e-3
         num_memories = 1000
-        # cutoff_time = 0.01
+        cutoff_multiplier = 0.1
+        min_cutoff_time = cutoff_multiplier * params["T_DP"]
         length_list = np.linspace(0, 4400e3, num=96)
         plot_info = {}
         custom_length_lists = {}
@@ -63,21 +31,21 @@ if __name__ == "__main__":
         for i, length in enumerate(length_list):
             start_time = time()
             print("----------")
-            cutoff_time = max(0.01, 4 * length / C)
+            cutoff_time = max(min_cutoff_time, 4 * length / C)
             p, w = run(length=length, max_iter=1000, params=params, cutoff_time=cutoff_time, num_memories=num_memories, return_world=True)
             key_per_time = standard_bipartite_evaluation(p.data)[2]
-            run_time = (time()-start_time)
+            run_time = (time() - start_time)
             print(f"{length=} finished in {run_time:.2f} seconds.")
             print(f"{key_per_time=}")
             print("Event stats:")
             w.event_queue.print_stats()
             keys += [key_per_time]
-            run_times +=[run_time]
+            run_times += [run_time]
             if key_per_time < 1e-1:
                 break
         plot_info = {}
-        plot_info["lengths"] = length_list[:i+1]
-        custom_length_list = length_list[:i+1]
+        plot_info["lengths"] = length_list[:i + 1]
+        custom_length_list = length_list[:i + 1]
         plot_info["keys"] = keys
         plot_info["run_times"] = run_times
         # save custom length_lists
@@ -94,7 +62,7 @@ if __name__ == "__main__":
         plt.ylabel("Key rate")
         plt.grid()
         # plt.legend()
-        plt.savefig(os.path.join(output_path,f"keys_{case_number}.png"))
+        plt.savefig(os.path.join(output_path, f"keys_{case_number}.png"))
         plt.close()
         # now plot run_times
         x = plot_info["lengths"]
@@ -104,7 +72,7 @@ if __name__ == "__main__":
         plt.ylabel("run time [s]")
         plt.grid()
         # plt.legend()
-        plt.savefig(os.path.join(output_path,f"run_times_{case_number}.png"))
+        plt.savefig(os.path.join(output_path, f"run_times_{case_number}.png"))
         plt.close()
     elif case_number in [6]:
         # Case 2: The memory quality plot - x-axis: length, thing we want to vary: t_dp
@@ -120,28 +88,29 @@ if __name__ == "__main__":
             print("%=====================%")
             print(f"{dephasing_time=}")
             params = dict(base_params)
-            params["DIVERGENCE_THETA"] = 5e-6
             params["T_DP"] = dephasing_time
+            cutoff_multiplier = 0.1
+            min_cutoff_time = cutoff_multiplier * params["T_DP"]
             keys = []
             run_times = []
             for i, length in enumerate(length_list):
                 start_time = time()
                 print("----------")
-                cutoff_time = max(0.1 * dephasing_time, 4 * length / C)
+                cutoff_time = max(min_cutoff_time, 4 * length / C)
                 p, w = run(length=length, max_iter=1000, params=params, cutoff_time=cutoff_time, num_memories=num_memories, return_world=True)
                 key_per_time = standard_bipartite_evaluation(p.data)[2]
-                run_time = (time()-start_time)
+                run_time = (time() - start_time)
                 print(f"{length=} finished in {run_time:.2f} seconds.")
                 print(f"{key_per_time=}")
                 print("Event stats:")
                 w.event_queue.print_stats()
                 keys += [key_per_time]
-                run_times +=[run_time]
+                run_times += [run_time]
                 if key_per_time < 1e-1:
                     break
             plot_info[dephasing_time] = {}
-            plot_info[dephasing_time]["lengths"] = length_list[:i+1]
-            custom_length_lists[dephasing_time] = length_list[:i+1]
+            plot_info[dephasing_time]["lengths"] = length_list[:i + 1]
+            custom_length_lists[dephasing_time] = length_list[:i + 1]
             plot_info[dephasing_time]["keys"] = keys
             plot_info[dephasing_time]["run_times"] = run_times
         # save custom length_lists
@@ -159,7 +128,7 @@ if __name__ == "__main__":
         plt.ylabel("Key rate")
         plt.grid()
         plt.legend()
-        plt.savefig(os.path.join(output_path,f"keys_{case_number}.png"))
+        plt.savefig(os.path.join(output_path, f"keys_{case_number}.png"))
         plt.close()
         # now plot run_times
         for dephasing_time in dephasing_times:
@@ -170,14 +139,14 @@ if __name__ == "__main__":
         plt.ylabel("run time [s]")
         plt.grid()
         plt.legend()
-        plt.savefig(os.path.join(output_path,f"run_times_{case_number}.png"))
+        plt.savefig(os.path.join(output_path, f"run_times_{case_number}.png"))
         plt.close()
     elif case_number == 7:
         # Case 2: The memory quality plot - x-axis: length, thing we want to vary: t_dp
         total_begin_time = time()
         num_memories = 1000
-        # cutoff_time = 0.01
-        length_list = np.linspace(0, 4400e3, num=96)
+        cutoff_multiplier = 0.1
+        length_list = np.linspace(0, 8800e3, num=96)
         orbital_heights = [400e3, 600e3, 1000e3, 1500e3, 2000e3]
         plot_info = {}
         custom_length_lists = {}
@@ -185,29 +154,28 @@ if __name__ == "__main__":
             print("%=====================%")
             print(f"{orbital_height=}")
             params = dict(base_params)
-            params["DIVERGENCE_THETA"] = 2e-6
-            params["T_DP"] = 100e-3
             params["ORBITAL_HEIGHT"] = orbital_height
+            min_cutoff_time = cutoff_multiplier * params["T_DP"]
             keys = []
             run_times = []
             for i, length in enumerate(length_list):
                 start_time = time()
                 print("----------")
-                cutoff_time = max(0.01, 4 * length / C)
+                cutoff_time = max(min_cutoff_time, 4 * length / C)
                 p, w = run(length=length, max_iter=1000, params=params, cutoff_time=cutoff_time, num_memories=num_memories, return_world=True)
                 key_per_time = standard_bipartite_evaluation(p.data)[2]
-                run_time = (time()-start_time)
+                run_time = (time() - start_time)
                 print(f"{length=} finished in {run_time:.2f} seconds.")
                 print(f"{key_per_time=}")
                 print("Event stats:")
                 w.event_queue.print_stats()
                 keys += [key_per_time]
-                run_times +=[run_time]
+                run_times += [run_time]
                 if key_per_time < 1e-1:
                     break
             plot_info[orbital_height] = {}
-            plot_info[orbital_height]["lengths"] = length_list[:i+1]
-            custom_length_lists[orbital_height] = length_list[:i+1]
+            plot_info[orbital_height]["lengths"] = length_list[:i + 1]
+            custom_length_lists[orbital_height] = length_list[:i + 1]
             plot_info[orbital_height]["keys"] = keys
             plot_info[orbital_height]["run_times"] = run_times
         # save custom length_lists
@@ -225,7 +193,7 @@ if __name__ == "__main__":
         plt.ylabel("Key rate")
         plt.grid()
         plt.legend()
-        plt.savefig(os.path.join(output_path,f"keys_{case_number}.png"))
+        plt.savefig(os.path.join(output_path, f"keys_{case_number}.png"))
         plt.close()
         # now plot run_times
         for orbital_height in orbital_heights:
@@ -236,5 +204,76 @@ if __name__ == "__main__":
         plt.ylabel("run time [s]")
         plt.grid()
         plt.legend()
-        plt.savefig(os.path.join(output_path,f"run_times_{case_number}.png"))
+        plt.savefig(os.path.join(output_path, f"run_times_{case_number}.png"))
+        plt.close()
+    elif case_number == 9:
+        total_begin_time = time()
+        length = 4400e3
+        cutoff_multiplier = 0.1
+        num_memories = 1000
+        orbital_heights = [400e3, 600e3, 1000e3, 1500e3, 2000e3]
+        labels = [int(x / 1000) for x in orbital_heights]
+        plot_info = {}
+        custom_variations = {}
+        variations = np.linspace(0, 0.4, num=96 // 2 + 1)
+        multipliers = [0.5 + x for x in variations]
+        for orbital_height, label in zip(orbital_heights, labels):
+            print("%=====================%")
+            print(f"orbital height={label}km")
+            keys = []
+            run_times = []
+            params = dict(base_params)
+            params["ORBITAL_HEIGHT"] = orbital_height
+            min_cutoff_time = cutoff_multiplier * params["T_DP"]
+            cutoff_time = max(min_cutoff_time, 4 * length / C)
+            for i, position_multiplier in enumerate(multipliers):
+                start_time = time()
+                print("----------")
+                p, w = run(length=length, max_iter=100, params=params,
+                           cutoff_time=cutoff_time, num_memories=num_memories,
+                           position_multiplier=position_multiplier,
+                           return_world=True)
+                key_per_time = standard_bipartite_evaluation(p.data)[2]
+                run_time = (time() - start_time)
+                print(f"{position_multiplier=} finished in {run_time:.2f} seconds.")
+                print(f"{key_per_time=}")
+                print("Event stats:")
+                w.event_queue.print_stats()
+                keys += [key_per_time]
+                run_times += [run_time]
+                if key_per_time < 1e-1:
+                    break
+            plot_info[orbital_height] = {}
+            plot_info[orbital_height]["variations"] = variations[:i + 1]
+            used_variations = variations[:i + 1]
+            custom_variations[orbital_height] = np.concatenate([-np.flip(used_variations[1:]), used_variations])
+            plot_info[orbital_height]["keys"] = keys
+            plot_info[orbital_height]["run_times"] = run_times
+        # save custom length_lists
+        with open(os.path.join(output_path, f"custom_variations_{case_number}.pickle"), "wb") as f:
+            pickle.dump(custom_variations, f)
+        print("%=======================================%")
+        print(f"The whole case {case_number} finished exploring in {(time() - total_begin_time) / 60:.2f} minutes.")
+        # now plot keys
+        for orbital_heights, label in zip(orbital_height, labels):
+            x = plot_info[orbital_height]["variations"]
+            y = plot_info[orbital_height]["keys"]
+            plt.scatter(x, y, s=10, label=f"orbital_height={label}km")
+        plt.yscale("log")
+        plt.xlabel("offset")
+        plt.ylabel("Key rate")
+        plt.grid()
+        plt.legend()
+        plt.savefig(os.path.join(output_path, f"keys_{case_number}.png"))
+        plt.close()
+        # now plot run_times
+        for orbital_height, label in zip(orbital_height, labels):
+            x = plot_info[orbital_height]["variations"]
+            y = plot_info[orbital_height]["run_times"]
+            plt.scatter(x, y, s=10, label=f"orbital_height={label}km")
+        plt.xlabel("offset")
+        plt.ylabel("run time [s]")
+        plt.grid()
+        plt.legend()
+        plt.savefig(os.path.join(output_path, f"run_times_{case_number}.png"))
         plt.close()
--- scenarios/one_satellite/multi_memory_satellite.py
+++ scenarios/one_satellite/multi_memory_satellite.py
@@ -1,70 +1,17 @@
 import os, sys; sys.path.insert(0, os.path.abspath("."))
-from quantum_objects import SchedulingSource, Source, Station
+from quantum_objects import SchedulingSource, Station
 from protocol import TwoLinkProtocol
 from world import World
-from events import SourceEvent, EntanglementSwappingEvent, EntanglementPurificationEvent
+from events import EntanglementSwappingEvent
 import libs.matrix as mat
 import numpy as np
-from libs.aux_functions import apply_single_qubit_map, y_noise_channel, z_noise_channel, w_noise_channel
-from warnings import warn
-from collections import defaultdict
+from libs.aux_functions import apply_single_qubit_map, w_noise_channel, distance
+from scenarios.three_satellites.common_functions import alpha_of_eta, construct_dephasing_noise_channel, construct_y_noise_channel, eta_dif, eta_atm, elevation_curved
 from noise import NoiseModel, NoiseChannel
-import pandas as pd
+from warnings import warn
+from consts import AVERAGE_EARTH_RADIUS as R_E
 from consts import SPEED_OF_LIGHT_IN_VACCUM as C
 from functools import lru_cache
-
-def construct_dephasing_noise_channel(dephasing_time):
-    def lambda_dp(t):
-        return (1 - np.exp(-t / dephasing_time)) / 2
-
-    def dephasing_noise_channel(rho, t):
-        return z_noise_channel(rho=rho, epsilon=lambda_dp(t))
-
-    return dephasing_noise_channel
-
-
-def construct_y_noise_channel(epsilon):
-    return lambda rho: y_noise_channel(rho=rho, epsilon=epsilon)
-
-
-def construct_w_noise_channel(epsilon):
-    return lambda rho: w_noise_channel(rho=rho, alpha=(1 - epsilon))
-
-
-def alpha_of_eta(eta, p_d):
-    return eta * (1 - p_d) / (1 - (1 - eta) * (1 - p_d)**2)
-
-
-def eta_dif(distance, divergence_half_angle, sender_aperture_radius, receiver_aperture_radius):
-    # calculated by simple geometry, because gaussian effects do not matter much
-    x = sender_aperture_radius + distance * np.tan(divergence_half_angle)
-    arriving_fraction = receiver_aperture_radius**2 / x**2
-    if arriving_fraction > 1:
-        warn("Aperture and divergence angle values might be off.")
-        arriving_fraction = 1
-    return arriving_fraction
-
-
-def eta_atm(elevation):
-    # eta of pi/2 to the power of csc(theta), equation (A4) in https://arxiv.org/abs/2006.10636
-    # eta of pi/2 (i.e. straight up) is ~0.8 for 780nm wavelength.
-    return 0.8**(1 / np.sin(elevation))
-
-
-def sat_dist_curved(ground_dist, h):
-    R_E = 6378e3
-    alpha = ground_dist / R_E
-    L = np.sqrt(R_E**2 + (R_E + h)**2 - 2 * R_E * (R_E + h) * np.cos(alpha))
-    return L
-
-
-def elevation_curved(ground_dist, h):
-    R_E = 6378e3
-    alpha = ground_dist / R_E
-    L = np.sqrt(R_E**2 + (R_E + h)**2 - 2 * R_E * (R_E + h) * np.cos(alpha))
-    beta = np.arcsin(R_E / L * np.sin(alpha))
-    gamma = np.pi - alpha - beta
-    return gamma - np.pi / 2
 
 
 class MultiMemoryProtocol(TwoLinkProtocol):
@@ -117,7 +64,8 @@ class MultiMemoryProtocol(TwoLinkProtocol):
             self.check()
 
 
-def run(length, max_iter, params, cutoff_time=None, num_memories=1, mode="sim", return_world=False):
+def run(length, max_iter, params, cutoff_time=None, num_memories=1,
+        position_multiplier=0.5, mode="sim", return_world=False):
     # unpack the parameters
     try:
         P_LINK = params["P_LINK"]
@@ -159,29 +107,59 @@ def run(length, max_iter, params, cutoff_time=None, num_memories=1, mode="sim", 
         DIVERGENCE_THETA = params["DIVERGENCE_THETA"]
     except KeyError as e:
         raise Exception('params["DIVERGENCE_THETA"] is a mandatory argument').with_traceback(e.__traceback__)
+    try:
+        POINTING_ERROR_SIGMA = params["POINTING_ERROR_SIGMA"]
+    except KeyError:
+        warn('params["POINTING_ERROR_SIGMA"] is not defined, assuming zero.')
+        POINTING_ERROR_SIGMA = 0
 
-    ELEVATION_THETA = elevation_curved(length / 2, ORBITAL_HEIGHT)
-    GROUND_SATELLITE_DISTANCE = sat_dist_curved(length / 2, ORBITAL_HEIGHT)
-    arrival_chance = (eta_dif(distance=GROUND_SATELLITE_DISTANCE,
-                              divergence_half_angle=DIVERGENCE_THETA,
-                              sender_aperture_radius=SENDER_APERTURE_RADIUS,
-                              receiver_aperture_radius=RECEIVER_APERTURE_RADIUS)
-                      * eta_atm(elevation=ELEVATION_THETA))
+    def position_from_angle(radius, angle):
+        return radius * np.array([np.sin(angle), np.cos(angle)])
+
+    station_a_angle = 0
+    station_a_position = position_from_angle(R_E, station_a_angle)
+    station_central_angle = position_multiplier * length / R_E
+    station_central_position = position_from_angle(R_E + ORBITAL_HEIGHT, station_central_angle)
+    station_b_angle = length / R_E
+    station_b_position = position_from_angle(R_E, station_b_angle)
+    elevation_left = elevation_curved(ground_dist=np.abs(station_central_angle - station_a_angle) * R_E, h=ORBITAL_HEIGHT)
+    elevation_right = elevation_curved(ground_dist=np.abs(station_b_angle - station_central_angle) * R_E, h=ORBITAL_HEIGHT)
+    arrival_chance_left = (eta_dif(distance=distance(station_a_position, station_central_position),
+                                   divergence_half_angle=DIVERGENCE_THETA,
+                                   sender_aperture_radius=SENDER_APERTURE_RADIUS,
+                                   receiver_aperture_radius=RECEIVER_APERTURE_RADIUS,
+                                   pointing_error_sigma=POINTING_ERROR_SIGMA)
+                           * eta_atm(elevation=elevation_left))
+    arrival_chance_right = (eta_dif(distance=distance(station_central_position, station_b_position),
+                                    divergence_half_angle=DIVERGENCE_THETA,
+                                    sender_aperture_radius=SENDER_APERTURE_RADIUS,
+                                    receiver_aperture_radius=RECEIVER_APERTURE_RADIUS,
+                                    pointing_error_sigma=POINTING_ERROR_SIGMA)
+                            * eta_atm(elevation=elevation_right))
 
     def imperfect_bsm_err_func(four_qubit_state):
         return LAMBDA_BSM * four_qubit_state + (1 - LAMBDA_BSM) * mat.reorder(mat.tensor(mat.ptrace(four_qubit_state, [1, 2]), mat.I(4) / 4), [0, 2, 3, 1])
 
-    def time_distribution(source):
+    def time_distribution_left(source):
         comm_distance = np.max([np.abs(source.position - source.target_stations[0].position), np.abs(source.position - source.target_stations[1].position)])
         comm_time = 2 * comm_distance / C
-        eta = P_LINK * arrival_chance
+        eta = P_LINK * arrival_chance_left
+        eta_effective = 1 - (1 - eta) * (1 - P_D)**2
+        trial_time = T_P + comm_time  # I don't think that paper uses latency time and loading time?
+        random_num = np.random.geometric(eta_effective)
+        return random_num * trial_time, random_num
+
+    def time_distribution_right(source):
+        comm_distance = np.max([np.abs(source.position - source.target_stations[0].position), np.abs(source.position - source.target_stations[1].position)])
+        comm_time = 2 * comm_distance / C
+        eta = P_LINK * arrival_chance_right
         eta_effective = 1 - (1 - eta) * (1 - P_D)**2
         trial_time = T_P + comm_time  # I don't think that paper uses latency time and loading time?
         random_num = np.random.geometric(eta_effective)
         return random_num * trial_time, random_num
 
     @lru_cache()  # CAREFUL: only makes sense if positions and errors do not change!
-    def state_generation(source):
+    def state_generation_left(source):
         state = np.dot(mat.phiplus, mat.H(mat.phiplus))
         comm_distance = np.max([np.abs(source.position - source.target_stations[0].position), np.abs(source.position - source.target_stations[1].position)])
         storage_time = 2 * comm_distance / C
@@ -189,28 +167,48 @@ def run(length, max_iter, params, cutoff_time=None, num_memories=1, mode="sim", 
             if station.memory_noise is not None:  # dephasing that has accrued while other qubit was travelling
                 state = apply_single_qubit_map(map_func=station.memory_noise, qubit_index=idx, rho=state, t=storage_time)
             if station.dark_count_probability is not None:  # dark counts are handled here because the information about eta is needed for that
-                eta = P_LINK * arrival_chance
+                eta = P_LINK * arrival_chance_left
+                state = apply_single_qubit_map(map_func=w_noise_channel, qubit_index=idx, rho=state, alpha=alpha_of_eta(eta=eta, p_d=station.dark_count_probability))
+        return state
+
+    @lru_cache()  # CAREFUL: only makes sense if positions and errors do not change!
+    def state_generation_right(source):
+        state = np.dot(mat.phiplus, mat.H(mat.phiplus))
+        comm_distance = np.max([np.abs(source.position - source.target_stations[0].position), np.abs(source.position - source.target_stations[1].position)])
+        storage_time = 2 * comm_distance / C
+        for idx, station in enumerate(source.target_stations):
+            if station.memory_noise is not None:  # dephasing that has accrued while other qubit was travelling
+                state = apply_single_qubit_map(map_func=station.memory_noise, qubit_index=idx, rho=state, t=storage_time)
+            if station.dark_count_probability is not None:  # dark counts are handled here because the information about eta is needed for that
+                eta = P_LINK * arrival_chance_right
                 state = apply_single_qubit_map(map_func=w_noise_channel, qubit_index=idx, rho=state, alpha=alpha_of_eta(eta=eta, p_d=station.dark_count_probability))
         return state
 
     misalignment_noise = NoiseChannel(n_qubits=1, channel_function=construct_y_noise_channel(epsilon=E_MA))
 
     world = World()
-    station_A = Station(world, position=0, memory_noise=None,
+    station_A = Station(world, position=station_a_position, memory_noise=None,
                         creation_noise_channel=misalignment_noise,
                         dark_count_probability=P_D
                         )
-    station_B = Station(world, position=2 * GROUND_SATELLITE_DISTANCE, memory_noise=None,
-                        creation_noise_channel=misalignment_noise,
-                        dark_count_probability=P_D
-                        )
-    station_central = Station(world, position=GROUND_SATELLITE_DISTANCE,
+    station_central = Station(world, position=station_central_position,
                               memory_noise=construct_dephasing_noise_channel(dephasing_time=T_DP),
                               memory_cutoff_time=cutoff_time,
                               BSM_noise_model=NoiseModel(channel_before=NoiseChannel(n_qubits=4, channel_function=imperfect_bsm_err_func))
                               )
-    source_A = SchedulingSource(world, position=GROUND_SATELLITE_DISTANCE, target_stations=[station_A, station_central], time_distribution=time_distribution, state_generation=state_generation)
-    source_B = SchedulingSource(world, position=GROUND_SATELLITE_DISTANCE, target_stations=[station_central, station_B], time_distribution=time_distribution, state_generation=state_generation)
+    station_B = Station(world, position=station_b_position, memory_noise=None,
+                        creation_noise_channel=misalignment_noise,
+                        dark_count_probability=P_D
+                        )
+
+    source_A = SchedulingSource(world, position=station_central_position,
+                                target_stations=[station_A, station_central],
+                                time_distribution=time_distribution_left,
+                                state_generation=state_generation_left)
+    source_B = SchedulingSource(world, position=station_central_position,
+                                target_stations=[station_central, station_B],
+                                time_distribution=time_distribution_right,
+                                state_generation=state_generation_right)
     protocol = MultiMemoryProtocol(world, num_memories=num_memories)
     protocol.setup()
 
@@ -243,5 +241,5 @@ if __name__ == "__main__":
     res = [standard_bipartite_evaluation(p.data) for p in ps]
     plt.errorbar(length_list / 1000, [r[4] / 2 for r in res], yerr=[r[5] / 2 for r in res], fmt="o")  # 10 * np.log10(key_per_resource))
     plt.yscale("log")
-    plt.legend()
+    # plt.legend()
     plt.show()
