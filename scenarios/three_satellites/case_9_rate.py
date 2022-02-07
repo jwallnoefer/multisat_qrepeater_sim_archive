bddb1407cc097f9a5d0327f49aef98f9485dde79
Julius Wallnöfer <julius.wallnoefer@outlook.com>
2022-02-07 18:37:49 +0100

    adds optimization option for effective rate analysis

--- scenarios/three_satellites/case_9_rate.py
+++ scenarios/three_satellites/case_9_rate.py
@@ -30,21 +30,39 @@ def raw_errors_from_data_frame(data_frame):
     return raw_rate, e_z, e_x
 
 
+def effective_rate_from_raw_errors(data_frame, orbital_period, err_corr_ineff=1):
+    integrand_x = data_frame["raw_rate"] * data_frame["e_x"]
+    integrand_z = data_frame["raw_rate"] * data_frame["e_z"]
+    normalization = np.trapz(y=data_frame["raw_rate"], x=data_frame.index)
+    effective_e_x = np.trapz(y=integrand_x, x=data_frame.index) / normalization
+    effective_e_z = np.trapz(y=integrand_z, x=data_frame.index) / normalization
+    effective_yield = normalization / orbital_period
+    return effective_yield * (1 - binary_entropy(effective_e_x) - err_corr_ineff * binary_entropy(effective_e_z))
+
+
 def rate_from_data_series(data_series, orbital_height, ground_distance, err_corr_ineff=1):
     offsets = np.array(data_series.index)
     angles = offsets * ground_distance / R_E
     orbital_period = 2 * np.pi * np.sqrt((R_E + orbital_height)**3 / (M * G))
     times = angles / (2 * np.pi) * orbital_period
     data = [list(raw_errors_from_data_frame(df)) for df in data_series]
-    new_series = pd.DataFrame(data, index=times, columns=["raw_rate", "e_z", "e_x"])
-    # now do the integration
-    integrand_x = new_series["raw_rate"] * new_series["e_x"]
-    integrand_z = new_series["raw_rate"] * new_series["e_z"]
-    normalization = np.trapz(y=new_series["raw_rate"], x=new_series.index)
-    effective_e_x = np.trapz(y=integrand_x, x=new_series.index) / normalization
-    effective_e_z = np.trapz(y=integrand_z, x=new_series.index) / normalization
-    effective_yield = normalization / orbital_period
-    return effective_yield * (1 - binary_entropy(effective_e_x) - err_corr_ineff * binary_entropy(effective_e_z))
+    new_df = pd.DataFrame(data, index=times, columns=["raw_rate", "e_z", "e_x"])
+    return effective_rate_from_raw_errors(data_frame=new_df, orbital_period=orbital_period, err_corr_ineff=err_corr_ineff)
+
+
+def optimize_rate_from_data_series(data_series, orbital_height, ground_distance, err_corr_ineff=1):
+    offsets = np.array(data_series.index)
+    angles = offsets * ground_distance / R_E
+    orbital_period = 2 * np.pi * np.sqrt((R_E + orbital_height)**3 / (M * G))
+    times = angles / (2 * np.pi) * orbital_period
+    data = [list(raw_errors_from_data_frame(df)) for df in data_series]
+    new_df = pd.DataFrame(data, index=times, columns=["raw_rate", "e_z", "e_x"])
+    shortened_dfs = [new_df] + [new_df.iloc[i:-i] for i in range(1, len(new_df) // 2)]
+    effective_rates = [effective_rate_from_raw_errors(data_frame=df,
+                                                      orbital_period=orbital_period,
+                                                      err_corr_ineff=err_corr_ineff)
+                       for df in shortened_dfs]
+    return np.max(effective_rates)
 
 
 # first do the twolink cases
@@ -54,12 +72,18 @@ ground_distance = 4400e3
 configurations = [-1, 0, 1, 2]
 paths = [os.path.join(result_path, f"{configuration}_configuration") for configuration in configurations]
 output = []
+# for path in paths:
+#     print("Now processing: ", path)
+#     data_series = pd.read_pickle(os.path.join(path, "raw_data.bz2"))
+#     output += [rate_from_data_series(data_series, orbital_height=orbital_height, ground_distance=ground_distance)]
+# res = pd.Series(data=output, index=configurations)
+# res.to_csv(os.path.join(result_path, "effective_rate.csv"))
 for path in paths:
     print("Now processing: ", path)
     data_series = pd.read_pickle(os.path.join(path, "raw_data.bz2"))
-    output += [rate_from_data_series(data_series, orbital_height=orbital_height, ground_distance=ground_distance)]
+    output += [optimize_rate_from_data_series(data_series, orbital_height=orbital_height, ground_distance=ground_distance)]
 res = pd.Series(data=output, index=configurations)
-res.to_csv(os.path.join(result_path, "effective_rate.csv"))
+res.to_csv(os.path.join(result_path, "optimized_effective_rate.csv"))
 
 # then same for fourlink case
 result_path = os.path.join("results", "three_satellites", "fourlink", "satellite_path")
@@ -68,9 +92,15 @@ ground_distance = 4400e3
 configurations = [-1, 0, 1, 2]
 paths = [os.path.join(result_path, f"{configuration}_configuration") for configuration in configurations]
 output = []
+# for path in paths:
+#     print("Now processing: ", path)
+#     data_series = pd.read_pickle(os.path.join(path, "raw_data.bz2"))
+#     output += [rate_from_data_series(data_series, orbital_height=orbital_height, ground_distance=ground_distance)]
+# res = pd.Series(data=output, index=configurations)
+# res.to_csv(os.path.join(result_path, "effective_rate.csv"))
 for path in paths:
     print("Now processing: ", path)
     data_series = pd.read_pickle(os.path.join(path, "raw_data.bz2"))
-    output += [rate_from_data_series(data_series, orbital_height=orbital_height, ground_distance=ground_distance)]
+    output += [optimize_rate_from_data_series(data_series, orbital_height=orbital_height, ground_distance=ground_distance)]
 res = pd.Series(data=output, index=configurations)
-res.to_csv(os.path.join(result_path, "effective_rate.csv"))
+res.to_csv(os.path.join(result_path, "optimized_effective_rate.csv"))
